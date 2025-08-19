d83b6b8cbd898062c7cbf36cd8e42c532026837c
Antony Chan <antony.sigma@gmail.com>
2025-08-18 21:32:35 -0700

    Demo app

---
+++ main.cpp
@@ -0,0 +1,157 @@
+// Dear ImGui: standalone example application for GLUT/FreeGLUT + OpenGL2, using legacy fixed
+// pipeline
+
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
+
+// !!! GLUT/FreeGLUT IS OBSOLETE PREHISTORIC SOFTWARE. Using GLUT is not recommended unless you
+// really miss the 90's. !!!
+// !!! If someone or something is teaching you GLUT today, you are being abused. Please show some
+// resistance. !!!
+// !!! Nowadays, prefer using GLFW or SDL instead!
+
+// On Windows, you can install Freeglut using vcpkg:
+//   git clone https://github.com/Microsoft/vcpkg
+//   cd vcpkg
+//   bootstrap - vcpkg.bat
+//   vcpkg install freeglut --triplet=x86-windows   ; for win32
+//   vcpkg install freeglut --triplet=x64-windows   ; for win64
+//   vcpkg integrate install                        ; register include and libs in Visual Studio
+
+#include "imgui.h"
+#include "imgui_impl_glut.h"
+#include "imgui_impl_opengl2.h"
+#define GL_SILENCE_DEPRECATION
+#include <GL/freeglut.h>
+
+namespace {
+// Our state
+static bool show_another_window = false;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+static float f = 0.0f;
+static int counter = 0;
+
+void
+MainLoopStep() {
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+    ImGui::NewFrame();
+    ImGuiIO& io = ImGui::GetIO();
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named
+    // window.
+    {
+        ImGui::Begin(
+            "Hello, world!");  // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text(
+            "This is some useful text.");  // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f,
+                           1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color",
+                          (float*)&clear_color);  // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))  // Buttons return true when clicked (most widgets return true
+                                      // when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate,
+                    io.Framerate);
+        ImGui::End();
+    }
+
+    // 3. Show another simple window.
+    if (show_another_window) {
+        ImGui::Begin(
+            "Another Window",
+            &show_another_window);  // Pass a pointer to our bool variable (the window will have a
+                                    // closing button that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me")) show_another_window = false;
+        ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+                 clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+struct GuiRuntime {
+    GuiRuntime() {
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+
+#ifdef __FREEGLUT_EXT_H__
+        glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+#endif
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+        glutInitWindowSize(1280, 720);
+        glutCreateWindow("Dear ImGui GLUT+OpenGL2 Example");
+
+        // Setup GLUT display function
+        // We will also call ImGui_ImplGLUT_InstallFuncs() to get all the other functions installed
+        // for us, otherwise it is possible to install our own functions and call the
+        // imgui_impl_glut.h functions ourselves.
+        glutDisplayFunc(MainLoopStep);
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        // ImGui::StyleColorsLight();
+
+        // Setup Platform/Renderer backends
+        // FIXME: Consider reworking this example to install our own GLUT funcs + forward calls
+        // ImGui_ImplGLUT_XXX ones, instead of using ImGui_ImplGLUT_InstallFuncs().
+        ImGui_ImplGLUT_Init();
+        ImGui_ImplOpenGL2_Init();
+
+        // Install GLUT handlers (glutReshapeFunc(), glutMotionFunc(), glutPassiveMotionFunc(),
+        // glutMouseFunc(), glutKeyboardFunc() etc.) You can read the io.WantCaptureMouse,
+        // io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main
+        // application, or clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main
+        // application, or clear/overwrite your copy of the keyboard data. Generally you may always
+        // pass all inputs to dear imgui, and hide them from your application based on those two
+        // flags.
+        ImGui_ImplGLUT_InstallFuncs();
+    }
+
+    ~GuiRuntime() {
+        // Cleanup
+        ImGui_ImplOpenGL2_Shutdown();
+        ImGui_ImplGLUT_Shutdown();
+        ImGui::DestroyContext();
+    }
+};
+}  // namespace
+
+int
+main(int argc, char** argv) {
+    // Create GLUT window
+    glutInit(&argc, argv);
+    GuiRuntime gui_runtime;
+
+    // Main loop
+    glutMainLoop();
+
+    return 0;
+}
